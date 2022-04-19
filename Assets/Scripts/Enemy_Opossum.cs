39850343f2f04ab05d3039e851d7820375e34b87
lc15225888231 <103026952+lc15225888231@users.noreply.github.com>
2022-04-19 16:15:56 +0800

    v1.3
    新地图的制作，老鼠的制作

---
+++ Assets/Scripts/Enemy_Opossum.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Opossum : Enemy
+{
+    private Rigidbody2D rb;
+    //private Animator Anim;
+    private Collider2D Coll;
+    public LayerMask Ground;
+    public Transform leftpoint, rightpoint;
+    public bool Faceleft = true;
+    public float Speed, JumpForce;
+    public float leftx, rightx;
+
+
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        base.Start();
+        rb = GetComponent<Rigidbody2D>();
+        // Anim = GetComponent<Animator>();
+        Coll = GetComponent<Collider2D>();
+
+
+        transform.DetachChildren();
+        leftx = leftpoint.position.x;
+        rightx = rightpoint.position.x;
+        Destroy(leftpoint.gameObject);
+        Destroy(rightpoint.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+
+    }
+
+
+    void Movement()
+    {
+        if (Faceleft)//面向左侧
+        {
+
+            rb.velocity = new Vector2(-Speed, JumpForce);
+            if (transform.position.x < leftx)//超过左侧的点转向
+            {
+                transform.localScale = new Vector3(-1, 1, 1);
+                Faceleft = false;
+            }
+        }
+        else
+        {
+            rb.velocity = new Vector2(Speed, JumpForce);
+            if (transform.position.x > rightx)//超过右侧的点转向
+            {
+                transform.localScale = new Vector3(1, 1, 1);
+                Faceleft = true;
+            }
+        }
+    }
+}
